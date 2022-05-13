ed7a523d9a5a258467da3b427e360f28206e72c8
AilenAdid <ailenadid95@gmail.com>
2022-05-13 15:53:33 -0300

    task resuelto

--- scripts/login.js
+++ scripts/login.js
@@ -42,7 +42,7 @@ window.addEventListener('load', function () {
     function realizarLogin(settings) {
         if (settings.jwt) {
             localStorage.setItem('jwt', JSON.stringify(settings.jwt));
-            location.replace('/mis-tareas.html');
+            location.replace('mis-tareas.html');
           } else {
             const span = document.querySelector(".campo-login");
             span.innerHTML= `${settings}`;
